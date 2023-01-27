dc88c2cab5a889876c747fb02aa5d75456cdf77d
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-01-26 21:08:58 -0800

    First commit

---
+++ server.js
@@ -0,0 +1,43 @@
+console.log ('May Node be with you')
+
+const express =  require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://sleepGuy:wdCRUD@cluster0.jdbzmzw.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.use(bodyParser.urlencoded({extended: true}))
+
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+    })
+    
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+    })
+
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
+  })
+  .catch(error => console.error(error))
+
+
+
+
+
+
+
+//mongoDB
+//sleepGuy
+//wdCRUD
