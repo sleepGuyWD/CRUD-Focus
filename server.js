05efb8244d79dc8e758f16db07e095985849a768
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-01-30 21:25:11 -0800

    Res.render

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ console.log ('May Node be with you')
 
 const express =  require('express')
 const bodyParser = require('body-parser')
+const { localsName } = require('ejs')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://sleepGuy:wdCRUD@cluster0.jdbzmzw.mongodb.net/?retryWrites=true&w=majority'
@@ -12,15 +13,18 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     const db = client.db('star-wars-quotes')
     const quotesCollection = db.collection('quotes')
 
+    app.set('view engine', 'ejs')
+
     app.use(bodyParser.urlencoded({extended: true}))
 
     app.get('/', (req, res) => {
       res.sendFile(__dirname + '/index.html')
       const cursor = db.collection('quotes').find().toArray()
       .then(results => {
-        console.log(results)
+        
       })
       .catch(error => console.error(error))
+      res.render('index.ejs', {quotes: results})
 
       //begin from here - "Using EJS and install"
     })
