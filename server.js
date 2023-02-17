f87953d50d9d0c4efcfab9cdc32332b5dcec2845
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-02-16 19:43:30 -0800

    server.js update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://sleepGuy:wdCRUD@cluster0.jdbzmzw.mongodb.net/?retryWrites=true&w=majority'
 const path = require('path')
+const PORT = 5555
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
@@ -67,7 +68,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
       .catch(error => console.error(error))
     })
 
-    app.listen(3000, function() {
+    app.listen(PORT, function() {
       console.log('listening on 3000')
     })
   })
