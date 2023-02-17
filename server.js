732daa09619b32c828da116e89dec2661f72ca84
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-02-16 20:01:07 -0800

    Port fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://sleepGuy:wdCRUD@cluster0.jdbzmzw.mongodb.net/?retryWrites=true&w=majority'
 const path = require('path')
-const port = process.env.PORT || 5555
+const PORT = process.env.PORT || 5555
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
@@ -68,8 +68,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
       .catch(error => console.error(error))
     })
 
-    app.listen(port, function() {
-      console.log('listening on 3000')
+    app.listen(PORT, function() {
+      console.log(`Listening on ${PORT}`)
     })
   })
   .catch(error => console.error(error))
